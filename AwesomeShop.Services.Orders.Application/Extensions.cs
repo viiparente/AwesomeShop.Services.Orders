e154a44faf6bba92f1f917ffdd7917fbbf36f16e
Vinicius Parente <vd.parente@hotmail.com>
2022-02-15 20:57:34 -0300

    CQRS - Created Commands and Queries

---
+++ AwesomeShop.Services.Orders.Application/Extensions.cs
@@ -0,0 +1,16 @@
+﻿using AwesomeShop.Services.Orders.Application.Commands;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AwesomeShop.Services.Orders.Application
+{
+    public static class Extensions
+    {
+        public static IServiceCollection AddHandlers(this IServiceCollection services)
+        {
+            services.AddMediatR(typeof(AddOrder));
+
+            return services;
+        }
+    }
+}
---
+++ AwesomeShop.Services.Orders.Application/Queries/Handlers/GetOrderByIdHandler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using AwesomeShop.Services.Orders.Application.Dtos.ViewModels;
+using AwesomeShop.Services.Orders.Core.Repositories;
+using MediatR;
+
+namespace AwesomeShop.Services.Orders.Application.Queries
+{
+    public class GetOrderByIdHandler : IRequestHandler<GetOrderById, OrderViewModel>
+    {
+        private readonly IOrderRepository _orderRepository;
+        public GetOrderByIdHandler(IOrderRepository orderRepository)
+        {
+            _orderRepository = orderRepository;
+        }
+
+        public async Task<OrderViewModel> Handle(GetOrderById request, CancellationToken cancellationToken)
+        {
+            var order = await _orderRepository.GetByIdAsync(request.Id);
+
+            var orderViewModel = OrderViewModel.FromEntity(order);
+
+            return orderViewModel;
+        }
+    }
+}
