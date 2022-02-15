3131b5f058563757b1ac0baf20876dc40a8cbe7b
Vinicius Parente <vd.parente@hotmail.com>
2022-02-14 21:34:29 -0300

    Clean Architecture - Core layer

---
+++ AwesomeShop.Services.Orders.Core/Entities/AggregateRoot.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using AwesomeShop.Services.Orders.Core.Events;
+
+namespace AwesomeShop.Services.Orders.Core.Entities
+{
+    public class AggregateRoot : IEntityBase
+    {
+        private readonly List<IDomainEvent> _events = new List<IDomainEvent>();
+        
+        public Guid Id { get; protected set; }
+        public List<IDomainEvent> Events => _events;
+
+        protected void AddEvent(IDomainEvent @event)
+        {
+            _events.Add(@event);
+        }
+    }
+}
